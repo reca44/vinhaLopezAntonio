5bf0231e0dfde64510f97785ff893608d7ab097d
vinlopant <antoniovilopez@gmail.com>
2022-02-23 10:45:01 +0100

    segundo commit

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -13,6 +13,24 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+             float dirX=Input.GetAxis("Horizontal");
+        float dirY=Input.GetAxis("Vertical");
         
+        Rigidbody2D rb=GetComponent<Rigidbody2D>();
+        //if(Input.GetButton("Jump")){
+          //  GetComponent<Rigidbody2D>().velocity=new Vector2(dirX*5,6);
+        //}else{
+        //rb.velocity=new Vector2(dirX*6,rb.velocity.y); 
+        //}
+
+           
+         Vector2 velocidad = new Vector2(dirX * 5, rb.velocity.y);
+ 
+        if (Input.GetButtonDown("Jump")) {
+            rb.AddForce(new Vector2(0, 500));
+        }
+ 
+        rb.velocity = velocidad;
     }
+    
 }
