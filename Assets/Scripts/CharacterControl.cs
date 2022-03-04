c0422527c0cd863994c92f4c0c208fc43239fef3
vinlopant <antoniovilopez@gmail.com>
2022-03-04 13:09:24 +0100

    girando

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
-{
+{ 
+    public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +14,9 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-             float dirX=Input.GetAxis("Horizontal");
+        float dirX=Input.GetAxis("Horizontal");
         float dirY=Input.GetAxis("Vertical");
-        
+        spriteRenderer.flipX=true;
         Rigidbody2D rb=GetComponent<Rigidbody2D>();
         //if(Input.GetButton("Jump")){
           //  GetComponent<Rigidbody2D>().velocity=new Vector2(dirX*5,6);
