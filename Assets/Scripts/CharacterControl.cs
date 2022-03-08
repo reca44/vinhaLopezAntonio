b5fcbbc206c3bbc2700f61f032e619f278950872
Reca <antoniovilopez@gmail.com>
2022-03-08 21:33:32 +0100

    fin con musica y una animacion

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
+    public Animator animator;
     public float velocidadCorrer = 6f;
     public float velocidadSalto = 18f;
     Rigidbody2D rb;
@@ -16,29 +17,31 @@ public class CharacterControl : MonoBehaviour
 
     void Update()
     {
-
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             spriteRenderer.flipX = false;
             rb.velocity = new Vector2(velocidadCorrer, rb.velocity.y);
-            
+            animator.SetBool("run", true);
+
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             spriteRenderer.flipX = true;
             rb.velocity = new Vector2(-velocidadCorrer, rb.velocity.y);
-            
+            animator.SetBool("run", true);
+
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
+            animator.SetBool("run", false);
         }
         if (Input.GetKey("space") && contacto.isGrounded)
         {
-            // rb.velocity = new Vector2(rb.velocity.x, velocidadSalto);
             rb.AddForce(new Vector2(0, 10));
 
         }
+        //intente hacer un doble salto pero no me funciona
         //if (Input.GetKey("space"))
         //{
         //    if (contacto.isGrounded)
@@ -60,58 +63,6 @@ public class CharacterControl : MonoBehaviour
         //        }
         //    }
         //}
-
-
-
-
-
-
-        //if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
-        //{
-        //    spriteRenderer.flipX = false;
-        //}
-        //else if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
-        //{
-        //    spriteRenderer.flipX = true;
-        //}
-
-
-
-
-
-
-        //if (Input.GetButtonDown("Jump"))
-        //{
-        //    if (contacto.isGrounded)
-        //    {
-        //        dobleSalto = true;
-        //        rb.AddForce(new Vector2(0, 250));
-        //    }
-        //    else
-        //    {
-        //        if (Input.GetButtonDown("Jump"))
-        //        {
-        //            if (dobleSalto)
-        //            {
-        //                rb.AddForce(new Vector2(0, 250));
-        //                dobleSalto = false;
-        //            }
-        //        }
-        //    }
-        //}
-
-
-        //Vector2 velocidad = /*/*new Vector2(dirX * 5, rb.velocity.y);*/*/
-
-
-
-        //if(dirX>0){
-        //  animator.Play("run",-1,0f);
-        //}else if(dirX<0){
-        //  animator.Play("idle",-1,0f);
-        //}
-
-        //rb.velocity = velocidad;
     }
 
 }
