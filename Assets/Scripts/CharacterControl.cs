72b6fe0ec90a2f2b49888b1768adf1da1d1ea1bb
vinlopant <antoniovilopez@gmail.com>
2022-03-08 09:01:31 +0100

    personaje girado

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 { 
     public SpriteRenderer spriteRenderer;
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         float dirX=Input.GetAxis("Horizontal");
         float dirY=Input.GetAxis("Vertical");
+        
         spriteRenderer.flipX=true;
         Rigidbody2D rb=GetComponent<Rigidbody2D>();
         //if(Input.GetButton("Jump")){
